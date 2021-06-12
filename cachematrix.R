a7cda2954e77cc8275e7142e1eb35afac3f2adf9
Gnosian-dataScience <jakub.bizan@hotmail.com>
2021-06-12 20:07:01 +0200

    assignment for Lexical Scope for getting the peer reviews

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,43 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
+###the function makeCacheMatrix creates a special "matrix", which is really a containing a function to
+###set the value of the matrix
+###get the value of the matrix
+###set the value of the inverse
+###get the value of the inverse
+
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        m <- NULL
+        set <- function(y) {
+                x <<- y
+                m <<- NULL
+        }
+        get <- function() x
+        setsolve <- function(solve) m <<- solve
+        getsolve <- function() m
+        list(set = set, get = get,
+             setsolve = setsolve,
+             getsolve = getsolve)
 }
 
 
 ## Write a short comment describing this function
+###the function is used to check if the inverse of the matrix is stored in the memory, 
+###if so, the value that was previously computed will be used.
+###if not, the inverse of the matrix will be computed.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        m <- x$getsolve()
+        if(!is.null(m)) {
+                message("getting cached data")
+                return(m)
+        }
+        data <- x$get()
+        m <- solve(data, ...)
+        x$setsolve(m)
+        m
 }
